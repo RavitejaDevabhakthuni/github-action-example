6ae8a5438fd5808f495cefa8d5fc3457e36e5c11
RavitejaDevabhakthuni <devabhakthuniraviteja@gmail.com>
2024-12-15 07:47:20 -0600

    code

---
+++ src/test/java/com/Demo/github_CICD_actions/GithubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Demo.github_CICD_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
